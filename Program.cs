f9c7a9d1833b8e0614bab811dd3bca7dbee84402
KhaNhi <KhaNhi@users.noreply.github.com>
2024-04-25 20:00:02 +0700

    v1

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Grass grass = new Grass();
+        grass.Efect();
+        Wood wood= new Wood();
+        wood.Efect();
+        Iron iron = new Iron();
+        iron.Efect();
+    }
+}
+
+public interface IDamager
+{
+    void Efect();
+}
+
+public class Wood : IDamager
+{
+    public void Efect()
+    {
+        Console.WriteLine("Tao lo tren go khi dan ban");
+    }
+}
+
+public class Iron : IDamager
+{
+    public void Efect()
+    {
+        Console.WriteLine("Tao ra lua khi dan ban");
+    }
+}
+
+
+
+public class Grass : IDamager
+{
+    public void Efect()
+    {
+        Console.WriteLine("Tao bun dat ban len khi dan ban");
+    }
+}
+public class Bulet
+{
+    Wood bulletCollider = new Wood();
+    //bulletCollider.Efect();
+}
